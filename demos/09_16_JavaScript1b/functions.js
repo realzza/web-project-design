88dde369f948eb7f957aaa224417908abff5c803
maxsym23 <maxsym@protonmail.com>
2021-09-16 13:43:09 -0400

    update demo files

---
+++ demos/09_16_JavaScript1b/functions.js
@@ -0,0 +1,48 @@
+/* Functions, Methods, and Events */
+
+/**
+ * Functions are blocks of code that are designed to perform a particular
+ * task.  The function is executed when something
+ * invokes it.
+ **/
+
+// defining a function
+
+// invoking a function
+
+// anonymous function
+
+// anonymous function as callback for another function
+
+// function scope
+
+// function arguments
+
+/*
+ * Methods are actions that can be performed on objects, or functions defined within objects
+ */
+
+// example of an object with a method
+
+// invoking the method
+
+/* JS also has a number of built in methods and objects that we've already discussed like the document object.  There is also the window object, the Math object, which has a number of helpful math methods (see line 59, above) */
+
+//Math object
+console.log(Math.round(77.89)); //round to integer
+console.log(Math.ceil(77.89)); //round up to integer
+console.log(Math.floor(77.89)); //round down to integer
+
+//see: https://www.w3schools.com/js/js_math.asp for all Math methods
+
+//Math.random() returns a random number between 0 and 1
+console.log(Math.random() * 100); //returns integers between 0 and 100
+
+/*
+ *  Events
+ * See MDN Listing of events and DOM objects associated: https://developer.mozilla.org/en-US/docs/Web/Events
+ */
+
+// call event with event handler "onclick"
+
+// addEventListener for "scoll" event
